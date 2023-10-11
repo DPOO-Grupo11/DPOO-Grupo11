07b5f803115041cecd7d2e99c0086bcc1fea9007
shmibblez <watchouthomes@gmail.com>
2023-10-11 13:56:40 -0500

    diagramas modificados

--- Entrega 3/Proyecto1/src/clases/Carro.java
+++ Entrega 3/Proyecto1/src/clases/Carro.java
@@ -7,7 +7,39 @@ class Carro {
   private String categoria;
   private Sede sede;
   private String estado;
+  private Cliente cliente;
   private LocalDate fechaRegresoMantenimiento;
   private ArrayList<Alquiler> historial;
-  
+
+  public Carro(String placa, String marca, String modelo, String color, String tipoDeTransmision, String categoia, Sede sede, String estado, Cliente cliente) {
+    this.placa = placa;
+    this.marca = marca;
+    this.modelo = modelo;
+    this.color = color;
+    this.tipoDeTransmision = tipoDeTransmision;
+    this.categoia = categoia;
+    this.sede = sede;
+    this.estado = estado;
+    this.cliente = cliente;
+  }
+
+  public void setEstado(String estado) {
+    this.estado = estado;
+  }
+
+  public void setFechaRegresoMantenimiento(LocalDate fecha) {
+    this.fechaRegresoMantenimiento = fecha;
+  }
+
+  public String getEstado() {
+    return this.estado;
+  }
+
+  public Cliente getClienteInfo() {
+    return this.cliente;
+  }
+
+  public Sede getSede() {
+    return this.sede;
+  }
 }
---
+++ Entrega 3/Proyecto1/src/clases/Sede.java
@@ -0,0 +1,59 @@
+class Sede  {
+  private String nombre;
+  private String ubicacion;
+  private LocalTime horaApertura;
+  private LocalTime horaCierre;
+  private ArrayList<String> diasApertura;
+
+  public Sede(   String nombre,
+   String ubicacion,
+   LocalTime horaApertura,
+   LocalTime horaCierre,
+   ArrayList<String> diasApertura ) {
+    this.nombre = nombre;
+    this.ubicacion = ubicacion;
+    this.horaApertura = horaApertura;
+    this.horaCierre = horaCierre;
+    this.diasApertura = diasApertura;
+   }
+
+  public void setNombre(String nombre) {
+    this.nombre = nombre;
+  }
+
+  public void setUbicacion(String ubicacion) {
+    this.ubicacion = ubicacion;
+  }
+
+  public void setHoraApertura(LocalTime horaApertura) {
+    this.horaApertura = horaApertura;
+  }
+
+  public void setHoraCierre(LocalTime horaCierre) {
+    this.horaCierre= horaCierre;
+  }
+
+  public void setDiasApertura(String diasApertura) {
+    this.diasApertura = diasApertura;
+  }
+
+  public String getNombre() {
+    return this.nombre;
+  }
+
+  public String getUbicacion() {
+    return this.ubicacion;
+  }
+
+  public LocalTime getHoraApertura() {
+    return this.horaApertura;
+  }
+
+  public LocalTime getHoraCierre() {
+    return this.horaCierre;
+  }
+
+  public ArrayList<String> getDiasApertura() {
+    return this.diasApertura;
+  }
+}
