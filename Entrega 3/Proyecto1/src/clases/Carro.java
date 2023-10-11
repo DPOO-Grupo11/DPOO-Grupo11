3cfb030d3dba1581a22849267139befa8a747804
shmibblez <watchouthomes@gmail.com>
2023-10-10 20:46:48 -0500

    clases agregadas

---
+++ Entrega 3/Proyecto1/src/clases/Carro.java
@@ -0,0 +1,13 @@
+class Carro {
+  private String placa;
+  private String marca;
+  private String modelo;
+  private String color;
+  private String tipoDeTransmision;
+  private String categoria;
+  private Sede sede;
+  private String estado;
+  private LocalDate fechaRegresoMantenimiento;
+  private ArrayList<Alquiler> historial;
+  
+}
