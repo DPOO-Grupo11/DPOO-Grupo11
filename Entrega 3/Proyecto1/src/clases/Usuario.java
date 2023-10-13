655d99705a052ef6b282079a06f4258ffe319b0d
shmibblez <watchouthomes@gmail.com>
2023-10-13 10:07:52 -0500

    cambios

--- Entrega 3/Proyecto1/src/clases/Usuario.java
+++ Entrega 3/Proyecto1/src/clases/Usuario.java
@@ -1,7 +1,44 @@
 package clases;
 
-public class Usuario {
+import java.io.Serializable;
+
+public abstract class Usuario implements Serializable {
+	private static final long serialVersionUID = 2531807322318657943L;
+	/**
+	 * 
+	 */
 	private String usuario;
 	private String contraseña;
 	private String tipo;
+
+	/*
+	 * getters
+	 */
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public String getContraseña() {
+		return contraseña;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+	
+	/*
+	 * setters
+	 */
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public void setContraseña(String contraseña) {
+		this.contraseña = contraseña;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
 }
