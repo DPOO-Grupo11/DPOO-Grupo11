c6a83fbe728017aca561c1fa4461db12f648babf
shmibblez <watchouthomes@gmail.com>
2023-11-14 15:55:34 -0500

    SistemaAlquiler arreglado

--- Proyecto2/Entrega 2/Proyecto2/src/interfaz/PaginaPrincipal.java
+++ Proyecto2/Entrega 2/Proyecto2/src/interfaz/PaginaPrincipal.java
@@ -1,5 +1,17 @@
 package interfaz;
 
-public class PaginaPrincipal {
+import javax.swing.JFrame;
+
+/**
+ * El login se hace aca
+ */
+public class PaginaPrincipal extends JFrame {
   // TODO: esta sera la pagina principal
+
+  public static void main(String[] args) {
+    PaginaPrincipal pp = new PaginaPrincipal();
+    pp.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+    pp.setResizable(false);
+    pp.setVisible(true);
+  }
 }
