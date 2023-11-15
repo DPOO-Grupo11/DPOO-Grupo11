e5e52a9893c3a0e2641c45162f16f08452965a75
HernanRamirezR <h.ramirezr@uniandes.edu.co>
2023-11-14 20:55:24 -0500

    menuadmin changes

--- Proyecto2/Entrega 2/Proyecto2/src/interfaz/menuadmin/MenuAdmin.java
+++ Proyecto2/Entrega 2/Proyecto2/src/interfaz/menuadmin/MenuAdmin.java
@@ -1,5 +1,17 @@
 package interfaz.menuadmin;
 
-public class MenuAdmin {
-//TODO MENU ADMIN ESTE ES EL CUADRO PRINCIPAL
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+
+public class MenuAdmin extends JFrame {
+
+	public MenuAdmin()
+	{
+		
+		setPreferredSize(new Dimension(500, 500));	
+	}
+	
 }
