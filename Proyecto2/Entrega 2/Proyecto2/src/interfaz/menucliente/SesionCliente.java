7de8cc7fd3982926b9eb75ff88c9293b4bf7e4b1
catalinavillacris <m.villacris@uniandes.edu.co>
2023-11-15 00:01:37 -0500

    menu cliente

--- Proyecto2/Entrega 2/Proyecto2/src/interfaz/menucliente/SesionCliente.java
+++ Proyecto2/Entrega 2/Proyecto2/src/interfaz/menucliente/SesionCliente.java
@@ -1,11 +1,13 @@
 package interfaz.menucliente;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class SesionCliente extends JFrame  {
 	
 	public SesionCliente() {
 		
+		JPanel panel= new JPanel();
 		
 		
 	}
