5fa2386d0c4abbf537604a6390a407f6b865328c
HernanRamirezR <h.ramirezr@uniandes.edu.co>
2023-11-14 21:30:13 -0500

    clean menu admin

--- Proyecto2/Entrega 2/Proyecto2/src/interfaz/menuadmin/MenuAdmin.java
+++ Proyecto2/Entrega 2/Proyecto2/src/interfaz/menuadmin/MenuAdmin.java
@@ -1,6 +1,5 @@
 package interfaz.menuadmin;
 
-<<<<<<< HEAD
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
@@ -14,11 +13,4 @@ public class MenuAdmin extends JFrame {
 		
 		setPreferredSize(new Dimension(500, 500));	
 	}
-	
-=======
-import javax.swing.JPanel;
-
-public class MenuAdmin extends JPanel {
-  // TODO MENU ADMIN ESTE ES EL CUADRO PRINCIPAL
->>>>>>> branch 'main' of https://github.com/DPOO-Grupo11/DPOO-Grupo3.git
 }
