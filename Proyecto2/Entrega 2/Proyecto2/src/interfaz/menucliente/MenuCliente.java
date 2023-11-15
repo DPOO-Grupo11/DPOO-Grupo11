766c905211d15b8bc0f7b8a7adbe056a48b47bae
catalinavillacris <m.villacris@uniandes.edu.co>
2023-11-14 23:59:52 -0500

    menu cliente

--- Proyecto2/Entrega 2/Proyecto2/src/interfaz/menucliente/MenuCliente.java
+++ Proyecto2/Entrega 2/Proyecto2/src/interfaz/menucliente/MenuCliente.java
@@ -1,7 +1,26 @@
 package interfaz.menucliente;
 
+import java.awt.Font;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
 public class MenuCliente extends JPanel {
+	
+	public MenuCliente() {
+		
+		JLabel titleLabel = new JLabel("Menu Cliente", SwingConstants.CENTER);
+		titleLabel.setFont(new Font("Helvetica", Font.BOLD, 16));
+		
+		JPanel buttonPanel= new JPanel();
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		
+		add(new JButton("Iniciar Sesion"));
+		add(new JButton("Registrarse"));
+		add(new JButton("Salir"));
+	}
 
 }
---
+++ Proyecto2/Entrega 2/Proyecto2/src/interfaz/menucliente/SesionCliente.java
@@ -0,0 +1,13 @@
+package interfaz.menucliente;
+
+import javax.swing.JFrame;
+
+public class SesionCliente extends JFrame  {
+	
+	public SesionCliente() {
+		
+		
+		
+	}
+
+}
