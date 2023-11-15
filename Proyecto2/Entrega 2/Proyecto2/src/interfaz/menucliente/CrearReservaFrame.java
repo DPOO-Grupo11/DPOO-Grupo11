d4b22018484a12f63f57bdd015273b671c2d21f3
catalinavillacris <m.villacris@uniandes.edu.co>
2023-11-15 14:57:11 -0500

    cambios interfaz clientes

---
+++ Proyecto2/Entrega 2/Proyecto2/src/interfaz/menucliente/CrearReservaFrame.java
@@ -0,0 +1,13 @@
+package interfaz.menucliente;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class CrearReservaFrame extends JFrame{
+	
+	public CrearReservaFrame() {
+		
+		JPanel panel=new JPanel();
+	}
+
+}
